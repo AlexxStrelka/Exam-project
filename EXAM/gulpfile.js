beefba2634282fe553c553f427577a40454f33b5
Oleksandra Strilenko <aleksandra_str@yahoo.com>
2016-07-22 19:04:11 +0300

    Components header, slider_block and meetPartners added

---
+++ EXAM/gulpfile.js
@@ -0,0 +1,77 @@
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var sass = require('gulp-ruby-sass');
+var autoPrefixer = require('gulp-autoprefixer');
+var cleanCSS = require('gulp-clean-css');
+var rename = require('gulp-rename');
+var rigger = require('gulp-rigger');
+var sourceMaps = require('gulp-sourcemaps');
+var server = require('gulp-server-livereload');
+var imageMin = require('gulp-imagemin');
+var cache = require('gulp-cache');
+var watch = require('gulp-watch');
+
+gulp.task('sass', function() {
+    return sass('src/main.scss')
+        .on('error', sass.logError)
+        // .pipe(sourceMaps.init({loadMaps: false}))
+        // .pipe(autoPrefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1'))
+        // .pipe(rename('app.css'))
+        // .pipe(cleanCSS())
+        // .pipe(sourceMaps.write())
+        .pipe(gulp.dest('build/css'));
+});
+
+gulp.task('pages', function(){
+    return gulp.src('src/*.html')
+        .pipe(gulp.dest('build'))
+});
+
+gulp.task('fonts', function(){
+    return gulp.src('src/fonts/*')
+        .pipe(gulp.dest('build/css/fonts'))
+});
+
+gulp.task('images', function(){
+    return gulp.src('src/img/*')
+        .pipe(gulp.dest('build/css/img'))
+});
+
+gulp.task('scripts', function() {
+    return gulp.src(['src/js/*.js'])
+        .pipe(concat('app.js', {newLine: ';'}))
+        .pipe(gulp.dest('build/js'));
+});
+
+gulp.task('webserver', function() {
+    gulp.src('build')
+        .pipe(server({
+            livereload: true,
+            directoryListing: false,
+            open: true,
+            log: 'info',
+            defaultFile: 'index.html'
+        }));
+});
+
+// gulp.task('stream', function () {
+//     // Endless stream mode 
+//     return watch('css/**/*.css', { ignoreInitial: false })
+//         .pipe(gulp.dest('build'));
+// });
+ 
+// gulp.task('callback', function () {
+//     // Callback mode, useful if any plugin in the pipeline depends on the `end`/`flush` event 
+//     return watch('css/**/*.css', function () {
+//         gulp.src('css/**/*.css')
+//             .pipe(gulp.dest('build'));
+//     });
+// });
+
+gulp.task('default', function() {
+    gulp.start('pages', 'sass', 'images', 'fonts', 'scripts');
+    gulp.watch('src/*.html', ['pages']);
+    gulp.watch('src/**/**/*.scss', ['sass']);
+    gulp.watch('src/img', ['images']);
+    gulp.watch('src/js/*.js', ['scripts']);
+});
---
+++ EXAM/src/js/script_carousel.js
@@ -0,0 +1,164 @@
+
+$(function() {
+    $('.jcarousel').jcarousel({
+        // Конфигурация инициализации карусели
+        wrap: 'circular'
+    });
+
+    $('.leftArrow').jcarouselControl({ target: '-=1' });
+    $('.rightArrow').jcarouselControl({ target: '+=1' });
+
+//Подключен дополнительный плагин jQuery Touchwipe для перелистывания на тачскрине
+ var $step1 = $('#step1');//Переменные для каждого блока со слайдером
+ var $step2 = $('#step2');
+ var $step3 = $('#step3');
+
+  $step1.touchwipe({
+    wipeLeft: function(event, direction, distance, duration, fingerCount) {   
+      $step1.jcarousel('scroll', '+=1');
+    },
+    wipeRight: function(event, direction, distance, duration, fingerCount) {
+      $step1.jcarousel('scroll', '-=1');
+    }
+  });
+
+  $step2.touchwipe({
+    wipeLeft: function(event, direction, distance, duration, fingerCount) {   
+      $step2.jcarousel('scroll', '+=1');
+    },
+    wipeRight: function(event, direction, distance, duration, fingerCount) {
+      $step2.jcarousel('scroll', '-=1');
+    }
+  });
+
+  $step3.touchwipe({
+    wipeLeft: function(event, direction, distance, duration, fingerCount) {   
+      $step3.jcarousel('scroll', '+=1');
+    },
+    wipeRight: function(event, direction, distance, duration, fingerCount) {
+      $step3.jcarousel('scroll', '-=1');
+    }
+  });
+
+
+
+
+    // $('.jcarousel-pagination').jcarouselPagination({
+    //     item: function(page) {
+    //         return '<a href="#' + page + '">' + page + '</a>';
+    //     }
+    // });
+
+		// $('.jcarousel').jcarouselAutoscroll({
+  //     interval: 3000,
+  //     target: '+=1',
+  //     autostart: true
+  // 	});
+
+  
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// window.onload = function(){
+
+     
+
+//     var block = window.document.getElementById("test"), // элемент
+
+//         anim,                                   // таймаут
+
+//         start,                                  // время старта
+
+//         now,                                        // текущее время
+
+//         duration = 1000,                            // продолжительность
+
+//         from = 0,                                   // стартовая позиция
+
+//         to = window.innerWidth/2,                   // финишная позиция
+
+//         progress = 0,                               // прогресс анимации
+
+//         x;                                      // позиция в текущий момент времени
+
+     
+
+//     // закон приращения аргумента (easing)
+
+//     function delta(param){
+
+//         return param;
+
+//     };
+
+     
+
+//     // рендер
+
+//     function render(){
+
+//         now = new Date().getTime();
+
+//         progress = (now-start)/duration;
+
+//         x = (to - from)*delta(progress) + from;
+
+         
+
+//         test.style.left = x+"px";
+
+         
+
+//         // если не конец выполняем анимацию еще
+
+//         if (progress < 1) anim = setTimeout(arguments.callee, 0)
+
+//             // иначе заканчиваем анимацию
+
+//             else
+
+//             {
+
+//                 clearTimeout(anim);
+
+//                 progress = 0;
+
+//             };
+
+//     };
+
+     
+
+//     window.onclick = function(){
+
+//         start = new Date().getTime();
+
+//         render();
+
+//     };
+
+ 
+
+// };
